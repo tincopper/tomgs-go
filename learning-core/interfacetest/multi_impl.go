cadd3e71c9a7a6e50016d5302ef23dab74ad32c6
tincopper <136082619@qq.com>
2020-03-19 10:15:24 +0800

    添加对go接口实现的理解

--- learning-core/interfacetest/multi_impl.go
+++ learning-core/interfacetest/multi_impl.go
@@ -11,6 +11,9 @@ type Closer interface {
 }
 
 // Socket 既实现了Writer接口也实现Closer接口
+// 因为Socket类型实现了io.Writer接口的所有方法（其实就是一个Write()函数），所以
+// Socket实现了接口io.Write
+// 所以从这里也可以理解，go的实现是怎么一个过程，只要实现了接口的方法，那么就实现了该接口，而不需要像Java里面的implements关键字来实现接口。
 type Socket struct {
 }
 
