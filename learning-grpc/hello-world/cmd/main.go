63bf0db61092dc8b3e2c694d24b030616796529e
tincopper <136082619@qq.com>
2021-11-16 20:12:30 +0800

    add grpc demo

---
+++ learning-grpc/hello-world/cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	helloworldpb "tomgs-go/learning-grpc/hello-world/api"
+)
+
+type server struct {
+	helloworldpb.UnimplementedGreeterServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+}
+
+func main() {
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer()
+	// Attach the Greeter service to the server
+	helloworldpb.RegisterGreeterServer(s, &server{})
+	// Serve gRPC Server
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	log.Fatal(s.Serve(lis))
+}
