292bc1bae470ca874060c78e797d1638a1e42d35
tincopper <136082619@qq.com>
2021-12-06 15:09:11 +0800

    add grpc demo

---
+++ learning-grpc/hello-world/pkg/rpc/rpc.go
@@ -0,0 +1,52 @@
+package rpc
+
+/*import (
+	"context"
+	"fmt"
+	"gim/pkg/grpclib"
+	"gim/pkg/pb"
+	"tomgs-go/learning-grpc/hello-world/pkg/logger"
+
+	"google.golang.org/grpc/balancer/roundrobin"
+
+	"google.golang.org/grpc"
+)
+
+var (
+	LogicIntClient    pb.LogicIntClient
+	ConnectIntClient  pb.ConnectIntClient
+	BusinessIntClient pb.BusinessIntClient
+)
+
+func InitLogicIntClient(addr string) {
+	conn, err := grpc.DialContext(context.TODO(), addr, grpc.WithInsecure(), grpc.WithUnaryInterceptor(interceptor),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)))
+	if err != nil {
+		logger.Sugar.Error(err)
+		panic(err)
+	}
+
+	LogicIntClient = pb.NewLogicIntClient(conn)
+}
+
+func InitConnectIntClient(addr string) {
+	conn, err := grpc.DialContext(context.TODO(), addr, grpc.WithInsecure(), grpc.WithUnaryInterceptor(interceptor),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, grpclib.AddrPickerName)))
+	if err != nil {
+		logger.Sugar.Error(err)
+		panic(err)
+	}
+
+	ConnectIntClient = pb.NewConnectIntClient(conn)
+}
+
+func InitBusinessIntClient(addr string) {
+	conn, err := grpc.DialContext(context.TODO(), addr, grpc.WithInsecure(), grpc.WithUnaryInterceptor(interceptor),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)))
+	if err != nil {
+		logger.Sugar.Error(err)
+		panic(err)
+	}
+
+	BusinessIntClient = pb.NewBusinessIntClient(conn)
+}*/
