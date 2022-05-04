8af61aa539755df61f76d6262cded76510cb0c87
tincopper <136082619@qq.com>
2022-05-04 14:10:32 +0800

    log demo

--- learning-core/mylog/my_log.go
+++ learning-core/mylog/my_log.go
@@ -1,26 +1,27 @@
 package mylog
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 )
 
 // 自定义日志级别
 var (
-    Debug *log.Logger
-    Info *log.Logger
-    Warn *log.Logger
-    Error *log.Logger
+	Debug *log.Logger
+	Info  *log.Logger
+	Warn  *log.Logger
+	Error *log.Logger
 )
 
 func init() {
-    log.Println("my log init ...")
-    Debug = log.New(os.Stdout, "[DEBUG] ", log.Ldate | log.Ltime | log.Lshortfile)
-    Info  = log.New(os.Stdout, "[INFO] ", log.Ldate | log.Ltime | log.Lshortfile)
-    Warn  = log.New(os.Stdout, "[WARN] ", log.Ldate | log.Ltime | log.Lshortfile)
-    Error = log.New(os.Stderr, "[Error] ", log.Ldate | log.Ltime | log.Lshortfile)
+	log.Println("my log init ...")
+	Debug = log.New(os.Stdout, "[DEBUG] ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(os.Stdout, "[INFO] ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warn = log.New(os.Stdout, "[WARN] ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(os.Stderr, "[Error] ", log.Ldate|log.Ltime|log.Lshortfile)
+	log.Fatal()
 }
 
 func main() {
-    Debug.Println("debug info...")
+	Debug.Println("debug info...")
 }
